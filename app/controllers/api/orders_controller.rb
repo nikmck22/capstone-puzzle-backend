c89c45af35da52d3e87bc94cbe226cfa8e1390ae
Nikki <nikmck22@gmail.com>
2020-05-10 17:54:45 -0400

    orders, carted_puzzles, routes and controllers

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -1,2 +1,27 @@
 class Api::OrdersController < ApplicationController
+  before_action :authenticate_user
+  
+  def index
+    @orders = current_user.orders
+    render "index.json.jb"    
+  end
+
+  def show
+    @order = Order.find_by(id: params[:id])
+    render "show.json.jb"
+  end
+  
+  def create
+    @carted_puzzles = CartedPuzzle.where(user_id: current_user.id).where(status: 'carted')
+    
+
+    @order = Order.new(
+      user_id: current_user.id,
+    )
+
+    @order.save
+    @carted_puzzles.update_all(status: 'ordered', order_id: @order.id)
+    
+    render "show.json.jb"
+  end
 end
